bf6b2dcbd1c0fc15db5233e2487439e62101feeb
Pro <twisted.fall@gmail.com>
2018-05-21 10:25:05 +0200

    Rework Logger to own handler

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,15 @@
-use std::marker;
-use std::os::raw;
-
+use std::{fmt, hash, os::raw};
 use super::{
-	sys,
 	as_udata,
-	udata_as,
 	LogLevel,
+	sys,
+	udata_as,
 };
 use super::ffi_types::FFI;
 
-/// Wrapper around underlying `xmpp_log_t` struct.
+type LogHandler<'cb> = dyn FnMut(LogLevel, &str, &str) + 'cb;
+
+/// Wrapper around the underlying `xmpp_log_t` struct.
 ///
 /// The best option to get a logger is to call [`Logger::default()`]. It will return you a logger that
 /// is tied into Rust logging facility provided by [`log`] crate. This functionality is available when
@@ -22,34 +22,33 @@ use super::ffi_types::FFI;
 ///
 /// [`Logger::default()`]: struct.Logger.html#method.default
 /// [`log`]: https://crates.io/crates/log
-#[derive(Debug, Hash)]
 pub struct Logger<'cb> {
 	inner: *mut sys::xmpp_log_t,
 	owned: bool,
-	_callbacks: marker::PhantomData<&'cb fn()>,
-
+	handler: Box<LogHandler<'cb>>,
 }
 
 impl<'cb> Logger<'cb> {
 	/// Create a new custom logger.
 	///
 	/// The callback argument will be called every time a log message needs to be printed.
-	pub fn new<CB>(handler: &'cb CB) -> Logger<'cb>
+	pub fn new<CB>(handler: CB) -> Logger<'cb>
 		where
 			CB: FnMut(LogLevel, &str, &str) + 'cb,
 	{
+		let handler = Box::new(handler);
 		Logger::with_inner(Box::into_raw(Box::new(sys::xmpp_log_t {
 			handler: Some(Self::log_handler_cb::<CB>),
-			userdata: as_udata(handler) as *mut _,
-		})), true)
+			userdata: as_udata(&*handler) as *mut _,
+		})), handler, true)
 	}
 
 	#[inline]
-	fn with_inner(inner: *mut sys::xmpp_log_t, owned: bool) -> Logger<'cb> {
+	fn with_inner(inner: *mut sys::xmpp_log_t, handler: Box<LogHandler<'cb>>, owned: bool) -> Logger<'cb> {
 		if inner.is_null() {
 			panic!("Cannot allocate memory for Logger")
 		}
-		Logger { inner, owned, _callbacks: marker::PhantomData }
+		Logger { inner, owned, handler }
 	}
 
 	/// [xmpp_get_default_logger](http://strophe.im/libstrophe/doc/0.9.2/group___context.html#ga33abde406c7a057006b109cf1b23c8f8)
@@ -57,7 +56,11 @@ impl<'cb> Logger<'cb> {
 	/// This method returns default `libstrophe` logger that just outputs log lines to stderr. Use it
 	/// if you compile without `rust-log` feature and want a quick debug log output.
 	pub fn new_internal(log_level: LogLevel) -> Logger<'static> {
-		Logger::with_inner(unsafe { sys::xmpp_get_default_logger(log_level) }, false)
+		Logger::with_inner(
+			unsafe { sys::xmpp_get_default_logger(log_level) },
+			Box::new(|_, _, _| {}),
+			false,
+		)
 	}
 
 	extern "C" fn log_handler_cb<CB>(userdata: *const raw::c_void, level: sys::xmpp_log_level_t, area: *const raw::c_char, msg: *const raw::c_char)
@@ -76,16 +79,6 @@ impl<'cb> Logger<'cb> {
 	}
 }
 
-#[cfg(feature = "rust-log")]
-fn log_handler(log_level: LogLevel, area: &str, message: &str) {
-	match log_level {
-		LogLevel::XMPP_LEVEL_DEBUG => debug!("{}: {}", area, message),
-		LogLevel::XMPP_LEVEL_INFO => info!("{}: {}", area, message),
-		LogLevel::XMPP_LEVEL_WARN => warn!("{}: {}", area, message),
-		LogLevel::XMPP_LEVEL_ERROR => error!("{}: {}", area, message),
-	}
-}
-
 impl Default for Logger<'static> {
 	/// Return a new logger that logs to standard Rust logging facilities.
 	///
@@ -95,11 +88,14 @@ impl Default for Logger<'static> {
 	/// [`log`]: https://crates.io/crates/log
 	#[cfg(feature = "rust-log")]
 	fn default() -> Self {
-		// this trick allows us to get the real pointer to global function and not reference to
-		// the temporary local var (as in case with &log_handler)
-		let log_handler_ptr = Box::into_raw(Box::new(log_handler));
-		let out = Logger::new(unsafe { log_handler_ptr.as_mut().unwrap() });
-		out
+		Logger::new(|log_level, area, message| {
+			match log_level {
+				LogLevel::XMPP_LEVEL_DEBUG => debug!("{}: {}", area, message),
+				LogLevel::XMPP_LEVEL_INFO => info!("{}: {}", area, message),
+				LogLevel::XMPP_LEVEL_WARN => warn!("{}: {}", area, message),
+				LogLevel::XMPP_LEVEL_ERROR => error!("{}: {}", area, message),
+			}
+		})
 	}
 
 	/// Create a new default logger by calling [`new_internal()`] with debug log level.
@@ -113,15 +109,31 @@ impl Default for Logger<'static> {
 	}
 }
 
-impl<'cb> PartialEq for Logger<'cb> {
+impl PartialEq for Logger<'_> {
 	fn eq(&self, other: &Logger) -> bool {
 		self.inner == other.inner
 	}
 }
 
-impl<'cb> Eq for Logger<'cb> {}
+impl Eq for Logger<'_> {}
 
-impl<'cb> Drop for Logger<'cb> {
+impl fmt::Debug for Logger<'_> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		f.debug_struct("Logger")
+			.field("inner", &self.inner)
+			.field("owned", &self.owned)
+			.finish()
+	}
+}
+
+impl hash::Hash for Logger<'_> {
+	fn hash<H: hash::Hasher>(&self, state: &mut H) {
+		self.inner.hash(state);
+		self.owned.hash(state);
+	}
+}
+
+impl Drop for Logger<'_> {
 	fn drop(&mut self) {
 		if self.owned {
 			unsafe {
@@ -131,4 +143,4 @@ impl<'cb> Drop for Logger<'cb> {
 	}
 }
 
-unsafe impl<'cb> Send for Logger<'cb> {}
+unsafe impl Send for Logger<'_> {}
