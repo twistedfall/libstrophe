6df728a90a4aaa2125873b3aa494b7ff8033d976
Pro <twisted.fall@gmail.com>
2018-12-19 08:45:43 +0100

    Introduce OwnedStreamError that can implement failure::Fail

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use std::{
 	fmt,
 	result,
-	str,
+	sync::Mutex,
 };
 
 use crate::{
@@ -52,6 +52,38 @@ pub(crate) fn code_to_result(code: i32) -> EmptyResult {
 	}
 }
 
+fn error_type_to_str(typ: sys::xmpp_error_type_t) -> &'static str {
+	match typ {
+		sys::xmpp_error_type_t::XMPP_SE_BAD_FORMAT => "Bad format",
+		sys::xmpp_error_type_t::XMPP_SE_BAD_NS_PREFIX => "Bad namespace prefix",
+		sys::xmpp_error_type_t::XMPP_SE_CONFLICT => "Conflict",
+		sys::xmpp_error_type_t::XMPP_SE_CONN_TIMEOUT => "Connection timeout",
+		sys::xmpp_error_type_t::XMPP_SE_HOST_GONE => "Gone",
+		sys::xmpp_error_type_t::XMPP_SE_HOST_UNKNOWN => "Host unknown",
+		sys::xmpp_error_type_t::XMPP_SE_IMPROPER_ADDR => "Improper address",
+		sys::xmpp_error_type_t::XMPP_SE_INTERNAL_SERVER_ERROR => "Internal server error",
+		sys::xmpp_error_type_t::XMPP_SE_INVALID_FROM => "Invalid from",
+		sys::xmpp_error_type_t::XMPP_SE_INVALID_ID => "Invalid id",
+		sys::xmpp_error_type_t::XMPP_SE_INVALID_NS => "Invalid namespace",
+		sys::xmpp_error_type_t::XMPP_SE_INVALID_XML => "Invalid XML",
+		sys::xmpp_error_type_t::XMPP_SE_NOT_AUTHORIZED => "Not authorized",
+		sys::xmpp_error_type_t::XMPP_SE_POLICY_VIOLATION => "Policy violation",
+		sys::xmpp_error_type_t::XMPP_SE_REMOTE_CONN_FAILED => "Connection failed",
+		sys::xmpp_error_type_t::XMPP_SE_RESOURCE_CONSTRAINT => "Resource constraint",
+		sys::xmpp_error_type_t::XMPP_SE_RESTRICTED_XML => "Restricted XML",
+		sys::xmpp_error_type_t::XMPP_SE_SEE_OTHER_HOST => "See other host",
+		sys::xmpp_error_type_t::XMPP_SE_SYSTEM_SHUTDOWN => "System shutdown",
+		sys::xmpp_error_type_t::XMPP_SE_UNDEFINED_CONDITION => "Undefined condition",
+		sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_ENCODING => "Unsupported encoding",
+		sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_STANZA_TYPE => "Unsupported stanza type",
+		sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_VERSION => "Unsupported version",
+		sys::xmpp_error_type_t::XMPP_SE_XML_NOT_WELL_FORMED => "XML is not well formed",
+	}
+}
+
+/// Error of the stream. Inspect the `typ` for the specific error type. `text` contains additional
+/// text information about the error. `stanza` is the original error stanza sent by the server,
+/// most probably you don't need to process it because data from it is already in `typ` and `text`.
 #[derive(Debug)]
 pub struct StreamError<'t, 's> {
 	pub typ: sys::xmpp_error_type_t,
@@ -64,39 +96,70 @@ impl<'t> From<&'t sys::xmpp_stream_error_t> for StreamError<'t, 't> {
 		StreamError {
 			typ: inner.type_,
 			text: unsafe { FFI(inner.text as _).receive() },
-			stanza: unsafe { Stanza::from_inner_ref_mut(inner.stanza) }
+			stanza: unsafe { Stanza::from_inner_ref_mut(inner.stanza) },
 		}
 	}
 }
 
 impl fmt::Display for StreamError<'_, '_> {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let text_type = match self.typ {
-			sys::xmpp_error_type_t::XMPP_SE_BAD_FORMAT => "Bad format",
-			sys::xmpp_error_type_t::XMPP_SE_BAD_NS_PREFIX => "Bad namespace prefix",
-			sys::xmpp_error_type_t::XMPP_SE_CONFLICT => "Conflict",
-			sys::xmpp_error_type_t::XMPP_SE_CONN_TIMEOUT => "Connection timeout",
-			sys::xmpp_error_type_t::XMPP_SE_HOST_GONE => "Gone",
-			sys::xmpp_error_type_t::XMPP_SE_HOST_UNKNOWN => "Host unknown",
-			sys::xmpp_error_type_t::XMPP_SE_IMPROPER_ADDR => "Improper address",
-			sys::xmpp_error_type_t::XMPP_SE_INTERNAL_SERVER_ERROR => "Internal server error",
-			sys::xmpp_error_type_t::XMPP_SE_INVALID_FROM => "Invalid from",
-			sys::xmpp_error_type_t::XMPP_SE_INVALID_ID => "Invalid id",
-			sys::xmpp_error_type_t::XMPP_SE_INVALID_NS => "Invalid namespace",
-			sys::xmpp_error_type_t::XMPP_SE_INVALID_XML => "Invalid XML",
-			sys::xmpp_error_type_t::XMPP_SE_NOT_AUTHORIZED => "Not authorized",
-			sys::xmpp_error_type_t::XMPP_SE_POLICY_VIOLATION => "Policy violation",
-			sys::xmpp_error_type_t::XMPP_SE_REMOTE_CONN_FAILED => "Connection failed",
-			sys::xmpp_error_type_t::XMPP_SE_RESOURCE_CONSTRAINT => "Resource constraint",
-			sys::xmpp_error_type_t::XMPP_SE_RESTRICTED_XML => "Restricted XML",
-			sys::xmpp_error_type_t::XMPP_SE_SEE_OTHER_HOST => "See other host",
-			sys::xmpp_error_type_t::XMPP_SE_SYSTEM_SHUTDOWN => "System shutdown",
-			sys::xmpp_error_type_t::XMPP_SE_UNDEFINED_CONDITION => "Undefined condition",
-			sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_ENCODING => "Unsupported encoding",
-			sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_STANZA_TYPE => "Unsupported stanza type",
-			sys::xmpp_error_type_t::XMPP_SE_UNSUPPORTED_VERSION => "Unsupported version",
-			sys::xmpp_error_type_t::XMPP_SE_XML_NOT_WELL_FORMED => "XML is not well formed",
-		};
-		write!(f, "{}{}", text_type, self.text.map_or_else(|| "".into(), |x| format!(": {}", x)))
+		write!(f, "{}", error_type_to_str(self.typ)).and_then(|r| {
+			if let Some(text) = self.text {
+				write!(f, ": {}", text)
+			} else {
+				Ok(r)
+			}
+		})
 	}
 }
+
+impl StreamError<'_, '_> {
+	pub fn to_owned(&self) -> OwnedStreamError {
+		self.into()
+	}
+}
+
+/// Owned version of [`StreamError`]. `stanza` is guarded by Mutex to make the error type `Sync` to
+/// satisfy `failure::Fail` bounds.
+///
+/// [`StreamError`]: struct.StreamError.html
+#[derive(Debug)]
+pub struct OwnedStreamError {
+	pub typ: sys::xmpp_error_type_t,
+	pub text: Option<String>,
+	pub stanza: Mutex<Stanza>, // Mutex because failure requires Sync
+}
+
+impl From<&StreamError<'_, '_>> for OwnedStreamError {
+	fn from(s: &StreamError<'_, '_>) -> Self {
+		OwnedStreamError {
+			typ: s.typ,
+			text: s.text.map(|x| x.to_owned()),
+			stanza: Mutex::new(s.stanza.clone()),
+		}
+	}
+}
+
+impl fmt::Display for OwnedStreamError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", error_type_to_str(self.typ)).and_then(|r| {
+			if let Some(ref text) = self.text {
+				write!(f, ": {}", text)
+			} else {
+				Ok(r)
+			}
+		})
+	}
+}
+
+impl Clone for OwnedStreamError {
+	fn clone(&self) -> Self {
+		OwnedStreamError {
+			typ: self.typ,
+			text: self.text.clone(),
+			stanza: Mutex::new(self.stanza.lock().expect("Cannot lock Mutex for cloning").clone()),
+		}
+	}
+}
+
+impl ::std::error::Error for OwnedStreamError {}
